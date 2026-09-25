bcb1e798fe6f70f3549df427495a7c971d4148ba
mohamedhayoun <mohamedhayoungc@gmail.com>
2026-09-25 11:07:37 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='pv-club-civil-v2';
+const FILES=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{
+    if(res.ok && new URL(e.request.url).origin===location.origin){const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));}
+    return res;}).catch(()=>caches.match('index.html'))));
+});
